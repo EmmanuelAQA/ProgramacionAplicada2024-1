892d5fa263204f02536272e891a0f79d60c38d5b
EmmanuelAQA <90941394+EmmanuelAQA@users.noreply.github.com>
2024-02-12 07:43:11 -0500

    Add files via upload

---
+++ Listas/Listas practicas 4.py
@@ -0,0 +1,10 @@
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(type(list1))
+print(list1)
+print(type(list2))
+print(list2)
+print(type(list3))
+print(list3)
